619a6539c67ee51b852a05deb65dc497b57eaeb8
Sibong'ile Mshanga <sjmshanga@gmail.com>
2026-08-20 05:59:48 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+#import another_module
+#print(another_module_varible)
+
+#import turtle 
+#timmy = turtle.Turtle()
+
+from turtle import Turtle , Screen
+"""The Screen represents a window in which this turle is going to show up"""
+timmy=Turtle()
+print(timmy)
+timmy.foward(100)
+timmy.shape("turtle")
+timmy.colour("blue")
+my_screen=Screen()
+print(my_screen.canvheight)
